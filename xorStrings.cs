426ad9755c279d6df2d31b063dbf6d16b91f8361
Sergio Botter <30984687+SBotter@users.noreply.github.com>
2022-05-02 09:28:35 -0700

    Add files via upload

---
+++ xorStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank
+{
+    public class xorStrings
+    {
+        public string Result(string s, string t)
+        {
+            string result = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(s.Substring(i, 1) == t.Substring(i, 1))
+                {
+                    result += "0";
+                }
+                else
+                {
+                    result += "1";
+                }
+            }
+            return result;
+        }
+    }
+}
